f2c1d58b7a3f85a7255ce187b2e4d16713ff79fd
Renato Marques <78667317+marquesdsilva@users.noreply.github.com>
2021-06-13 14:31:37 -0300

    estudo de python

--- comeco.py
+++ comeco.py
@@ -1,3 +1,15 @@
 print("Seja bem vindo! ")
 nome = input("Qual e seu nome ")
 print("Prazer "+ nome)
+n1 = input("Digite umnumero ")
+n2 = input("Muito bem, digite outro numero ")
+soma = n1 + n2
+print("Legal, que tipo de operacao deseja fazer? ")
+print("Digite 1 para somar")
+print("Digite 2 para diminuir")
+print("Digite 3 para multiplicar")
+print("Digite 4 para dividir")
+op = input()
+
+if op == 1:
+ print(soma)
